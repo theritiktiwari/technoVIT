9504a2ea5a3e9b9a85cdabc8481665511d2075f3
Ritik Tiwari <ritiktiwariwork@gmail.com>
2022-08-13 15:55:18 +0530

    Backend Added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const STUDIO_REWRITE = {
+  source: "/admin/:path*",
+  destination:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3333/admin/:path*"
+      : "/admin/index.html",
+};
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  rewrites: () => [STUDIO_REWRITE],
 }
 
 module.exports = nextConfig
