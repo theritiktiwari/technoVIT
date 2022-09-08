9ee2a9b3016b327f0902635e32ebbe0221be9fce
Hariket Sukeshkumar Sheth <SHETHHARIKET@gmail.com>
2022-09-08 09:04:51 +0530

    Added School Events

--- Components/data/schoolData/bandvit.js
+++ Components/data/schoolData/bandvit.js
@@ -1,14 +1,24 @@
 const data = [
-    {
-        id: "1",
-        name: "Industry Conclave Final",
-        image: "https://i.ibb.co/Sxq0pr7/Industry-conclave-final.png"
-    },
-    {
-        id: "2",
-        name: "Robo Colloquy",
-        image: "https://i.ibb.co/2MhHC6S/ROBO-COLLOQUY.png"
-    },
-]
+  {
+    id: "1",
+    name: "Industry Conclave Final",
+    image: "https://i.ibb.co/Sxq0pr7/Industry-conclave-final.png",
+  },
+  {
+    id: "2",
+    name: "Robo Colloquy",
+    image: "https://i.ibb.co/2MhHC6S/ROBO-COLLOQUY.png",
+  },
+  {
+    id: "3",
+    name: "BioPrinting: An Introduction and Application",
+    image: "https://i.imgur.com/wy04uTA.jpg",
+  },
+  {
+    id: "4",
+    name: "Signathon",
+    image: "https://i.ibb.co/2MhHC6S/ROBO-COLLOQUY.png",
+  },
+];
 
 export default data;
--- Components/data/schoolData/diseno.js
+++ Components/data/schoolData/diseno.js
@@ -4,6 +4,16 @@ const data = [
         name: "E-no-wa(s)te",
         image: "https://i.ibb.co/yXCXRsp/e-no-waste-a4-poster.png"
     },
+    {
+        id: "2",
+        name: "Metaverse Workshop",
+        image: "https://i.imgur.com/6FBPN1i.jpg"
+    },
+    {
+        id: "3",
+        name: "Special Team Showcase",
+        image: "https://i.ibb.co/yXCXRsp/e-no-waste-a4-poster.png"
+    }
 ]
 
 export default data;
