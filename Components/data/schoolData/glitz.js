ff1730b28953f57d1ebfef9fbf330c51684718b9
Hariket Sukeshkumar Sheth <SHETHHARIKET@gmail.com>
2022-09-08 10:35:24 +0530

    All school events updated

--- Components/data/schoolData/glitz.js
+++ Components/data/schoolData/glitz.js
@@ -1,6 +1,78 @@
 const data = [
     {
-
+        id: "1",
+        name: "Draping Workshop",
+        image: "https://i.imgur.com/M8VfRuX.jpg",
+    },
+    {
+        id: "2",
+        name: "Fashion Illustration Workshop",
+        image: "https://i.imgur.com/JTLD9cj.jpg",
+    },
+    {
+        id: "3",
+        name: "Unisex Hairstyling & Makeover",
+        image: "https://i.imgur.com/sLyPTHh.jpg",
+    },
+    {
+        id: "4",
+        name: "Face Painting",
+        image: "https://i.imgur.com/0S3qU6y.jpg",
+    },
+    {
+        id: "5",
+        name: "Fashion Police",
+        image: "https://i.imgur.com/ost1n5u.jpg",
+    },
+    {
+        id: "6",
+        name: "Open Mic",
+        image: "https://i.imgur.com/yK3I2JW.png",
+    },
+    {
+        id: "7",
+        name: "Selfie Contest",
+        image: "https://i.imgur.com/0TnfieO.jpg",
+    },
+    {
+        id: "8",
+        name: "Styling and Makeover",
+        image: "https://i.imgur.com/MEkp4mu.jpg",
+    },
+    {
+        id: "9",
+        name: "Treasure Hunt",
+        image: "https://i.imgur.com/9ELmA6H.png",
+    },
+    {
+        id: "10",
+        name: "Pic-o-phactory",
+        image: "https://i.imgur.com/BGLmsoN.jpg",
+    },
+    {
+        id: "11",
+        name: "Drapester",
+        image: "https://i.imgur.com/7KmnBho.jpg",
+    },
+    {
+        id: "12",
+        name: "Fabric Embellishment",
+        image: "https://i.imgur.com/1oobWLx.jpg",
+    },
+    {
+        id: "13",
+        name: "Fashion Illustration",
+        image: "https://i.imgur.com/AK5k8oq.jpg",
+    },
+    {
+        id: "14",
+        name: "Poster Design",
+        image: "https://i.imgur.com/oXphB5K.jpg",
+    },
+    {
+        id: "15",
+        name: "Zero Waste Draping",
+        image: "https://i.imgur.com/ZwBE1LV.jpg",
     },
 ]
 
--- Components/data/schoolData/qubit.js
+++ Components/data/schoolData/qubit.js
@@ -1,35 +1,125 @@
 const data = [
-    {
-        id: "1",
-        name: "Live Hacking Hands on Workshop",
-        image: "https://i.ibb.co/s3H88Vj/Live-Hacking-Hands-on-Workshop-MIC-TECHNOVIT.png"
-    },
-    {
-        id: "2",
-        name: "Live ",
-        image: "https://i.ibb.co/s3H88Vj/Live-Hacking-Hands-on-Workshop-MIC-TECHNOVIT.png"
-    },
-    {
-        id: "3",
-        name: "Live Hacking Hands on Workshop",
-        image: "https://i.ibb.co/s3H88Vj/Live-Hacking-Hands-on-Workshop-MIC-TECHNOVIT.png"
-    },
-    {
-        id: "4",
-        name: "Live Hacking Hands on Workshop",
-        image: "https://i.ibb.co/s3H88Vj/Live-Hacking-Hands-on-Workshop-MIC-TECHNOVIT.png"
-    },
-    {
-        id: "5",
-        name: "Live Hacking Hands on Workshop",
-        image: "https://i.ibb.co/s3H88Vj/Live-Hacking-Hands-on-Workshop-MIC-TECHNOVIT.png"
-    },
-    {
-        id: "6",
-        name: "Live Hacking Hands on Workshop",
-        image: "https://i.ibb.co/s3H88Vj/Live-Hacking-Hands-on-Workshop-MIC-TECHNOVIT.png"
-    },
-
-]
+  {
+    id: "1",
+    name: "Live Hacking Hands on Workshop",
+    image:
+      "https://i.ibb.co/s3H88Vj/Live-Hacking-Hands-on-Workshop-MIC-TECHNOVIT.png",
+  },
+  {
+    id: "2",
+    name: "Skribbl Game-a-thon",
+    image: "https://i.imgur.com/FNG6GbX.png",
+  },
+  {
+    id: "3",
+    name: "Fall Guys Game-a-thon",
+    image: "https://i.imgur.com/bLeU6xT.png",
+  },
+  {
+    id: "4",
+    name: "Musical Chairs",
+    image: "https://i.imgur.com/gL8J2ll.jpg",
+  },
+  {
+    id: "5",
+    name: "Tug of War",
+    image: "https://i.imgur.com/zH1zW65.jpg",
+  },
+  {
+    id: "6",
+    name: "Valorant Game-a-thon",
+    image: "https://i.imgur.com/JWWmqhs.png",
+  },
+  {
+    id: "7",
+    name: "Bottle Flip Game-a-thon",
+    image: "https://i.imgur.com/B6QZ5MJ.jpg",
+  },
+  {
+    id: "8",
+    name: "COD Game-a-thon",
+    image: "https://i.imgur.com/WgSAxx3.jpg",
+  },
+  {
+    id: "9",
+    name: "Celebrating our Gurus",
+    image: "https://i.imgur.com/Ocy3EPM.png",
+  },
+  {
+    id: "10",
+    name: "AR VR Workshop",
+    image: "https://i.imgur.com/ZTsLHb7.jpg",
+  },
+  {
+    id: "11",
+    name: "Journey to Blockchain",
+    image: "https://i.imgur.com/39ysTyx.jpg",
+  },
+  {
+    id: "12",
+    name: "Coding Olympiad",
+    image: "https://i.imgur.com/Nc6GZIg.png",
+  },
+  {
+    id: "13",
+    name: "Data Analysis using Python",
+    image: "https://i.imgur.com/cPPG0F9.png",
+  },
+  {
+    id: "14",
+    name: "Session on Deep Learning",
+    image: "https://i.imgur.com/fUt50u1.jpg",
+  },
+  {
+    id: "15",
+    name: "Drone design",
+    image: "https://i.imgur.com/V7zXBAX.png",
+  },
+  {
+    id: "16",
+    name: "Build Apps with Flutter",
+    image: "https://i.imgur.com/4H9jJMI.jpg",
+  },
+  {
+    id: "17",
+    name: "How Machines Mimic Humans",
+    image: "https://i.imgur.com/C3xPjEp.jpg",
+  },
+  {
+    id: "18",
+    name: "Java Programming",
+    image: "https://i.imgur.com/8TlLKfx.jpg",
+  },
+  {
+    id: "19",
+    name: "MERN Stack Development",
+    image: "https://i.imgur.com/JevpYMT.jpg",
+  },
+  {
+    id: "20",
+    name: "Applied Natural Language Processing",
+    image: "https://i.imgur.com/hM1UccQ.png",
+  },
+  {
+    id: "21",
+    name: "Software Testing Selenium",
+    image: "https://i.imgur.com/CiLZNlh.jpg",
+  },
+  {
+    id: "22",
+    name: "UI UX Workshop (Day 1)",
+    image: "https://i.imgur.com/ZzgZuRW.jpg",
+  },
+  {
+    id: "23",
+    name: "UI UX Workshop (Day 2)",
+    image: "https://i.imgur.com/5ic8g5r.jpg",
+  },
+  {
+    id: "24",
+    name: "Technical Training on BERT",
+    image: "https://i.imgur.com/p50qpHn.jpg",
+  },
+];
 
 export default data;
