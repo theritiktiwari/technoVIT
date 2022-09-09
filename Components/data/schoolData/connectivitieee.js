2edb8196cca0a22d94fa45ccd0b5cd394888315d
Hariket Sukeshkumar Sheth <shethhariket@gmail.com>
2022-09-09 19:03:15 +0530

    Update connectivitieee.js

--- Components/data/schoolData/connectivitieee.js
+++ Components/data/schoolData/connectivitieee.js
@@ -1,6 +1,39 @@
 const data = [
     {
 
+        id: "1",
+        name: "PCB Workshop",
+        image: "https://i.imgur.com/6othiJF.png"
+    },
+    {
+        id: "2",
+        name: "Cyber Security",
+        image: "https://i.imgur.com/hhAxFkm.jpg"
+    },
+    {
+        id: "3",
+        name: "Hack-a-thon",
+        image: "https://i.imgur.com/7eg7xhH.jpg"
+    },
+    {
+        id: "4",
+        name: "Electrical Vehicle Technology Workshop",
+        image: "https://i.imgur.com/oFKYVU2.jpg"
+    },
+    {
+        id: "5",
+        name: "Labview Workshop",
+        image: "https://i.imgur.com/9I6AFgd.jpg"
+    },
+    {
+        id: "6",
+        name: "Ideathon",
+        image: "https://i.imgur.com/ThwRLNW.jpg"
+    },
+    {
+        id: "7",
+        name: "Make-a-thon",
+        image: "https://i.imgur.com/oyvfqwC.jpg"
     },
 ]
 
