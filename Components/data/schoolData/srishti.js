1337e1b1c9f18a6bb590b92d8d586158f638f3fa
Hariket Sukeshkumar Sheth <SHETHHARIKET@gmail.com>
2022-09-15 15:43:59 +0530

    Updated Srishti Events

--- Components/data/schoolData/srishti.js
+++ Components/data/schoolData/srishti.js
@@ -1,7 +1,64 @@
 const data = [
-    {
-
-    },
-]
+  {
+    id: "1",
+    name: "Short Film on Environmental Sustainability",
+    image: "https://i.imgur.com/GKfxbw5.jpg",
+  },
+  {
+    id: "2",
+    name: "3 Legged Race",
+    image: "https://i.imgur.com/ZKWTvNh.jpg",
+  },
+  {
+    id: "3",
+    name: "Sack Race",
+    image: "https://i.imgur.com/hONBMfL.jpg",
+  },
+  {
+    id: "4",
+    name: "Integrated Project Management",
+    image: "https://i.imgur.com/yNUDqxi.jpg",
+  },
+  {
+    id: "5",
+    name: "Tug Of War",
+    image: "https://i.imgur.com/I71fMUy.jpg",
+  },
+  {
+    id: "6",
+    name: "Civi-Quiz",
+    image: "https://i.imgur.com/kjgPQFG.jpg",
+  },
+  {
+    id: "7",
+    name: "Musical Chairs",
+    image: "https://i.imgur.com/n6y20Vt.jpg",
+  },
+  {
+    id: "8",
+    name: "Staad Pro Workshop",
+    image: "https://i.imgur.com/0rzJecA.jpg",
+  },
+  {
+    id: "9",
+    name: "Truss-A-Thon",
+    image: "https://i.imgur.com/M9Ezv1a.jpg",
+  },
+  {
+    id: "10",
+    name: "Problem Statement",
+    image: "https://i.imgur.com/x69frna.jpg",
+  },
+  {
+    id: "11",
+    name: "Arm Wrestling",
+    image: "https://i.imgur.com/0shnrzN.jpg",
+  },
+  {
+    id: "12",
+    name: "Opportunity in Civil Engineering",
+    image: "https://i.imgur.com/4QnyHDN.jpg",
+  },
+];
 
 export default data;
