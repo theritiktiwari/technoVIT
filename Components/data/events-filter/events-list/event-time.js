1a570f4967f5ce949da77a96ac49d67cc4ddd9cb
Hariket Sukeshkumar Sheth <SHETHHARIKET@gmail.com>
2022-09-18 21:16:59 +0530

    Updated Time

--- Components/data/events-filter/events-list/event-time.js
+++ Components/data/events-filter/events-list/event-time.js
@@ -3,8 +3,7 @@ import React from "react";
 function EventTime(props) {
   const { time } = props;
   // 2022-09-23T19:00:00.000Z
-  const humanReadableTime = new Date(time).toTimeString().slice(0, 9);
-
+  const humanReadableTime = time.slice(11,16);
   return <div>{humanReadableTime}</div>;
 }
 
