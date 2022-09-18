6535c81dcb5b73bf1e328cdf12b8d3d57c1fdd90
Hariket Sukeshkumar Sheth <SHETHHARIKET@gmail.com>
2022-09-18 09:48:10 +0530

    Updated Schedule

--- Components/data/schedule.js
+++ Components/data/schedule.js
@@ -2,17 +2,17 @@ const data = [
     {
         id: "1",
         name: "Day 1",
-        link: "",
+        link: "https://drive.google.com/file/d/1B91md35Wc7DPC-mNjTuph6h0T2b4Y40A/view?usp=sharing",
     },
     {
         id: "2",
         name: "Day 2",
-        link: ""
+        link: "https://drive.google.com/file/d/1CE_JX9MW0R-w1qnQ4UHY24RInid8xBdh/view?usp=sharing"
     },
     {
         id: "3",
         name: "Day 3",
-        link: ""
+        link: "https://drive.google.com/file/d/1j6_NfbUflLiLEFIEKjBsOqAFBTE7aDcM/view?usp=sharing"
     }
 ]
 
