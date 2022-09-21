02d83269c4c1adcdae93bfcf5f59fafcd38744ea
Hariket Sukeshkumar Sheth <SHETHHARIKET@gmail.com>
2022-09-21 21:11:22 +0530

    Added Conditions

--- Components/data/events-filter/conditions.js
+++ Components/data/events-filter/conditions.js
@@ -1,26 +1,32 @@
 import React from "react";
 import Sept23 from "../Sept23.json";
+import Sept24 from "../Sept24.json";
+import Sept25 from "../Sept25.json";
 import EventFiltered from "./events-list/event-filtered";
 
 function Conditions() {}
 
-export function getAllEvents23() {
-  return Sept23;
+export function getAllEvents() {
+  return [...Sept23, ...Sept24, ...Sept25];
 }
 
 export function getFilteredEvents(date, dept) {
   if (date === 0 && dept === 0) {
-    return Sept23;
+    return getAllEvents();
   } else if (date === 23 && dept === 0) {
     return Sept23;
+  } else if (date === 24 && dept === 0) {
+    return Sept24;
+  } else if (date === 25 && dept === 0) {
+    return Sept25;
   } else if (date === 0) {
-    return Sept23.filter((event) => event.resource === dept);
+    return getAllEvents().filter((event) => event.resource === dept);
   } else if (date === 23) {
     return Sept23.filter((event) => event.resource === dept);
   } else if (date === 24) {
-    return Sept23.filter((event) => event.resource === dept);
+    return Sept24.filter((event) => event.resource === dept);
   } else if (date === 25) {
-    return Sept23.filter((event) => event.resource === dept);
+    return Sept25.filter((event) => event.resource === dept);
   } else {
     return [];
   }
